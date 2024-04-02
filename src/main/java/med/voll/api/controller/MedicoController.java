7ea6e289a8b1daa4782b48b99974b06f8650a27c
Wenya-Teixeira <wenyatexprojetos@gmail.com>
2024-04-01 22:09:00 -0300

    Requisicoes PUT e Delete - Atualizando dados, delete e exclusao logica

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,11 +1,6 @@
 package med.voll.api.controller;
 
-
-
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedicos;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,7 +24,20 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+    }
+
+  @PutMapping
+  @Transactional
+  public void atualizar(@RequestBody @Validated DadosAtualizacaoMedico dados) {
+         var medico = repository.getReferenceById(dados.id());
+         medico.atualizarInformacoes(dados);
+    }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 
 
